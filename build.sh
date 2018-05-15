705a3c46af9fd965e843d9b7d072138f39bf74bc
ximing <ximing@meituan.com>
2018-05-15 16:58:32 +0800

    mark verson

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 rm -rf lib
 export NODE_ENV=production
-rsync -va -progress  ./src-lib/* ./lib
+#rsync -va -progress  ./src-lib/* ./lib
+./node_modules/.bin/minifier  ./src-lib/* ./lib
 babel src-lib --out-dir lib
 babel src-lib/index.js --out-file  lib/index.js
 ./node_modules/.bin/webpack --config webpack.build.lib.config.js --progress
